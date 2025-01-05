6405938b5243fa9dd16cf1ec9fa8c4fe2a9ff315
Emmaline261 <emmaline261@gmail.com>
2025-01-05 16:55:17 +0100

    changes based on the code review

--- wsb-jpa1/src/test/com/jpacourse/persistence/dao/impl/AddressDaoImplTest.java
+++ wsb-jpa1/src/test/com/jpacourse/persistence/dao/impl/AddressDaoImplTest.java
@@ -3,13 +3,16 @@ package com.jpacourse.persistence.dao.impl;
 import com.jpacourse.persistence.dao.AddressDao;
 import com.jpacourse.persistence.entity.AddressEntity;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @Transactional
 public class AddressDaoImplTest {
 
@@ -23,7 +26,7 @@ public class AddressDaoImplTest {
         AddressEntity addressEntity = addressDao.findOne(1L);
         // then
         assertThat(addressEntity).isNotNull();
-        assertThat(addressEntity.getPostalCode()).isEqualTo("62-030");
+        assertThat(addressEntity.getPostalCode()).isEqualTo("00-001");
     }
 
     @Test
