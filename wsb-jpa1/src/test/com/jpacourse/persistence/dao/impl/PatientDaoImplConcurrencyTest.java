4531f86b1a80cf492888d75d43cce10004633ae3
Emmaline261 <emmaline261@gmail.com>
2025-01-07 23:52:37 +0100

    added dirty context annotation to the concurrency test

--- wsb-jpa1/src/test/com/jpacourse/persistence/dao/impl/PatientDaoImplConcurrencyTest.java
+++ wsb-jpa1/src/test/com/jpacourse/persistence/dao/impl/PatientDaoImplConcurrencyTest.java
@@ -6,10 +6,12 @@ import org.hibernate.OptimisticLockException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class PatientDaoImplConcurrencyTest {
 
     @Autowired
